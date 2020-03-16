e854138fd9941b321162e77f68d69e9d6387edf6
bauyrzhan <probaumak@gmail.com>
2020-03-16 11:02:08 +0600

    Add turnstiles Functionality

---
+++ source/components/Modal/CreateTurnstile.vue
@@ -0,0 +1,141 @@
+<template lang='pug'>
+
+    div#create-dormitory( class='modal' )
+        h3( class='modal-title' ) Создание Турникета
+
+        form#form( class='blocks-div' @submit='save' )
+            div( class='inputs-div' )
+                InputText( class='create-input' placeholder='ID турникета' v-bind='{required: true}' v-model='turnstile.id' )
+                InputText( class='create-input' placeholder='SN турникета' v-bind='{required: true}' v-model='turnstile.sn' )
+                InputText( class='create-input' placeholder='Название общежития' v-bind='{required: true}' v-model='turnstile.dormitory' disabled )
+                InputText( class='create-input' placeholder='Название турникета' v-bind='{required: true}' v-model='turnstile.name' )
+
+            div( class='buttons-div' )
+                Button( title='Отменить' background='red' icon-top='2px' @click.native='cancel' )
+                Button( icon='create' title='Создать' background='green' icon-top='2px' )
+                
+
+</template>
+
+<script>
+import Button from ':src/components/UI/Button.vue'
+
+import InputText from ':src/components/UI/Input/Text.vue'
+import Select from ':src/components/UI/Input/Select.vue'
+
+export default {
+    props: ['id', 'data'],
+    components: { InputText, Select, Button },
+    methods: { defaults, save, cancel },
+    data: function () {
+        return {
+            turnstile: {
+                id: '',
+                sn:'',
+                dormitory: this.data.dormitory || '',
+                name: '',
+                active: true,
+            }
+        }
+    }
+}
+
+function defaults () {
+    return {
+        id: '',
+        sn:'',
+        dormitory: '',
+        name: ''
+    }
+}
+
+async function save (event) {
+    if ( event && event.preventDefault )
+        event.preventDefault()
+
+    let empty = Object.keys(this.turnstile).find(key => {
+        return this.turnstile[key] == false
+    })
+    console.log(empty)
+    if ( empty === undefined ) {
+        let { data, success } = await this.api.post('turnstile', this.turnstile)
+
+        if ( success === true ) {
+            this.$store.commit('dormitory-turnstiles-set', data)
+            this.dormitory = this.defaults()
+            
+            return this.$store.commit("closeModal", this.id)
+        }
+    }
+
+}
+
+function cancel () {
+    return this.$store.commit("closeModal", this.id)
+}
+</script>
+
+<style lang='stylus'>
+@import '~src/style/palette'
+
+#create-dormitory
+    padding 24px
+
+    .create-input
+        display block
+        margin 12px 0
+        width 100%
+
+    .buttons-div
+        border-top 1px solid lighten($light-gray, 15)
+        display flex
+        justify-content space-between
+        margin-top 8px
+        padding-top 16px
+        width 100%
+
+    #form
+        align-items flex-start
+        display flex
+        flex-direction column
+        justify-content space-between
+        margin-top 18px
+
+        .turns-div
+            margin-left 16px
+            position relative
+            top -14px
+            width 400px
+
+        .turns-title
+            color darken($light-gray, 25)
+            font-size 16px
+            font-weight 500
+
+        .turns-list
+            border 1px solid lighten($light-gray, 5)
+            border-radius 5px
+            margin-top 6px
+            padding 6px 0
+
+            .head
+                padding-bottom 6px
+
+                .data
+                    font-weight 500
+
+            .row
+                display flex
+                justify-content space-between
+
+            .data
+                padding 4px 12px
+                text-align left
+                width calc(100% - 40px - 140px - 3px)
+                &:first-child
+                    width 40px
+                &:last-child
+                    text-align right
+                    width 140px
+
+</style>
--- source/script/api.js
+++ source/script/api.js
@@ -1,7 +1,10 @@
 export default { install }
 
-const host = 'api.studdom.host'
-const port = 443
+// const host = 'api.studdom.host'
+const host = 'localhost:8000'
+
+// const port = 443
+const port = 8000
 
 // const host = 'localhost'
 // const port = 8000
@@ -12,7 +15,7 @@ function install (Vue) {
 
 function get (path, data = {}) {
     var query = getQuery(data)
-    var response = request(`https://${host}/${path}?${query}`, {
+    var response = request(`http://${host}/${path}?${query}`, {
         method: 'GET'
     })
 
@@ -21,7 +24,7 @@ function get (path, data = {}) {
 
 function post (path, data = {}) {
     var body = getFormData(data)
-    var response = request(`https://${host}/${path}`, {
+    var response = request(`http://${host}/${path}`, {
         body,
         method: 'POST'
     })
